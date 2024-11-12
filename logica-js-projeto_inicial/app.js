d52548862469b07ca7dd909563984a54723765ef
CodeFox121 <bruno.cordeiro.reis@escola.pr.gov.br>
2024-11-12 14:02:09 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert('Olá! :)');
+alert('Eu sou Robson, o robô! estou feliz em te conhecer.')
+alert('Seja muito bem vindo ao jogo do número secreto! isso vai ser divertido :)');
+alert('Eu estou pensando em um número, e você tem que tentar adivinhar!')
+let NumeroSecreto = 29;
+let chute = prompt('Vai, escolhe um número entre 1 e 30!')
+
+if(NumeroSecreto = chute) {
+    console.log('Aeeeeee!! você descobriu o número secreto :D')
+}
